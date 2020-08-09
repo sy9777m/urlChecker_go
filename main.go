542962c9faa7eba03bf66f7c11964b7623994f4f
Siyun Min <sy9777m@naver.com>
2020-08-09 23:40:16 +0900

    init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for url := range urls {
+		fmt.Println(url)
+	}
+}
